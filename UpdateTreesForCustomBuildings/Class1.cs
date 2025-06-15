58f1782c45626324377c150881235bcdc8909cb5
Geodomus <georg10102002@yahoo.de>
2025-06-15 05:36:31 +0200

    Fixed Project, now working as intended.

---
+++ UpdateTreesForCustomBuildings/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using UnityEngine;
+namespace UpdateTreesForCustomBuildings
+{
+    public class Class1
+    {
+        [StaticConstructorOnStartup]
+        public static int Init()
+        {
+            //Harmony.DEBUG = true;
+
+            var harmony = new Harmony("Destroy Completely Dismantled and Empty Building");
+
+            harmony.PatchAll();
+
+            return 2;
+        }
+        
+        [HarmonyPatch(typeof(Player), "OnTreeGrown")]
+        class Patch {
+		
+            static void Postfix(Player __instance)
+            {
+                int count = __instance.buildingsDict[(BuildingID)200].Count;
+                while (count-- > 0)
+                {
+                    if (__instance.buildingsDict[(BuildingID)200][count].IsIdle())
+                        __instance.buildingsDict[(BuildingID)200][count].ForceIdleCheck();
+                }
+            }
+        }
+    }
+}
